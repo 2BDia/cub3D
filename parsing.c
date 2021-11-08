97c3ced7ba86d91ecb50b9b195bf42298b696203
Julie Van Audenhaege <rvan-aud@fu-r2-p8.s19.be>
2021-11-08 17:24:13 +0100

    fixed bugs with spaces before and added space protection around it, todo : remove trailing spaces after right border in get_map

--- parsing.c
+++ parsing.c
@@ -1,93 +1,105 @@
 #include "cub3d.h"
 
-char	*get_each_texture(t_map *map, char *prefix, int offset)
+int	get_map_height(char **map_f)
 {
-	int	i;
-	int	j;
+	int	y;
 
-	i = 0;
-	while (map->raw_file[i])
-	{
-		j = 0;
-		while (map->raw_file[i][j] == ' ')
-			j++;
-		if (!ft_strncmp_space(prefix, map->raw_file[i], offset))
-		{
-			while (map->raw_file[i][offset + j] == ' ')
-				offset++;
-			return (map->raw_file[i] + (offset + j));
-		}
-		i++;
-	}
-	return (NULL);
+	y = 0;
+	while (map_f[y])
+		y++;
+	return (y);
 }
 
-int	get_textures(t_map *map)
+int	check_around(char **map_f, int x, int y, int height)
 {
-	map->n_texture = get_each_texture(map, "NO", 2);
-	map->s_texture = get_each_texture(map, "SO", 2);
-	map->w_texture = get_each_texture(map, "WE", 2);
-	map->e_texture = get_each_texture(map, "EA", 2);
-	map->f_color = get_each_texture(map, "F", 1);
-	map->c_color = get_each_texture(map, "C", 1);
-	if (!map->n_texture || !map->s_texture || !map->w_texture || !map->e_texture || !map->f_color || !map->c_color)
+	if (y == 0)
 	{
-		write(1, "Wrong texture path\n", 19);
-		return (1);
+		if (map_f[y + 1][x] == '1' || map_f[y + 1][x] == ' ')
+			if (map_f[y][x - 1] == '1' || map_f[y][x - 1] == ' ')
+				if (map_f[y][x + 1] == '1' || map_f[y][x + 1] == ' ')
+					return (0);
 	}
-	return (0);
-}
-
-int	check_only_spaces(char *line)
-{
-	int	i;
-
-	i = 0;
-	while (line[i])
+	else if (y == height)
 	{
-		if (line[i] != ' ' || line[i] != '\n')
-			return (0);
+		if (map_f[y - 1][x] == '1' || map_f[y - 1][x] == ' ')
+			if (map_f[y][x - 1] == '1' || map_f[y][x - 1] == ' ')
+				if (map_f[y][x + 1] == '1' || map_f[y][x + 1] == ' ')
+					return (0);
+	}
+	else
+	{
+		if (map_f[y - 1][x] == '1' || map_f[y - 1][x] == ' ')
+			if (map_f[y + 1][x] == '1' || map_f[y + 1][x] == ' ')
+				if (map_f[y][x - 1] == '1' || map_f[y][x - 1] == ' ')
+					if (map_f[y][x + 1] == '1' || map_f[y][x + 1] == ' ')
+						return (0);
 	}
 	return (1);
 }
 
-int	get_map(t_map *map)
+int	check_map(char **map_f)
 {
-	int	i;
-	int	save;
-	int	j;
+	int	x;
+	int	y;
+	int	start;
+	int	player;
+	int	height;
 
-	i = 0;
-	while (map->raw_file[i] && (map->raw_file[i][0] != '1' && map->raw_file[i][0] != '0'))
+	y = 0;
+	player = 0;
+	height = get_map_height(map_f) - 1;
+	while (map_f[y])
 	{
-		if (map->raw_file[i][0] == 'N' || map->raw_file[i][0] == 'S')
-			if (map->raw_file[i][1] != 'O')
-				break ;
-		if (map->raw_file[i][0] == 'W')
-			if (map->raw_file[i][1] != 'E')
-				break ;
-		if (map->raw_file[i][0] == 'E')
-			if (map->raw_file[i][1] != 'A')
-				break ;
-		i++;
-	}
-	save = i;
-	while (map->raw_file[i])
-	{
-		if (check_only_spaces(map->raw_file[i]))
+		start = 0;
+		x = 0;
+		if (y == 0 || y == height)
 		{
-			i--;
-			break ;
+			while (map_f[y][x] && x < (int)ft_strlen(map_f[y]))
+			{	
+				while (start == 0 && map_f[y][x] == ' ')
+					x++;
+				start = 1;
+				if (map_f[y][x] != '1' && map_f[y][x] != ' ')
+				{
+					return (1);
+				}
+				if (map_f[y][x] == ' ')
+					if (check_around(map_f, x, y, height))
+						return (1);
+				x++;
+			}
 		}
-		i++;
+		else
+		{
+			while (map_f[y][x] && x < (int)ft_strlen(map_f[y]))
+			{
+				while (start == 0 && map_f[y][x] == ' ')
+					x++;
+				if (start == 0 && map_f[y][x] != '1')
+					return (1);
+				start = 1;
+				if (x == (int)ft_strlen(map_f[y]) - 1 && map_f[y][x] != '1')
+					return (1);
+				if (map_f[y][x] != '0' && map_f[y][x] != '1' && map_f[y][x] != ' '
+					&& map_f[y][x] != 'N' && map_f[y][x] != 'S'
+					&& map_f[y][x] != 'W' && map_f[y][x] != 'E')
+					return (1);
+				if (map_f[y][x] == 'N' || map_f[y][x] == 'S'
+					|| map_f[y][x] == 'W' || map_f[y][x] == 'E')
+				{
+					if (player || y == 0 || y == height || x == 0 || x == (int)ft_strlen(map_f[y]))
+						return (1);
+					else
+						player = 1;
+				}
+				if (map_f[y][x] == ' ')
+					if (check_around(map_f, x, y, height))
+						return (1);
+				x++;
+			}
+		}
+		y++;
 	}
-	map->map_f = (char **)malloc(sizeof(char *) * (i - save) + 1);
-	if (!map->map_f)
-		return (1);
-	j = 0;
-	while (save <= i)
-		map->map_f[j++] = map->raw_file[save++];
-	map->map_f[j] = NULL;
 	return (0);
 }
 
@@ -97,6 +109,13 @@ int	parsing(char *argv, t_map *map)
 	if (get_textures(map))
 		return (1);
 	if (get_map(map))
+		return (1);
+	if (check_map(map->map_f))
+	{
+		write(1, "wrong map\n", 10);
+		return (1);
+	}
+	if (get_pos(map))
 		return (1);
 	return (0);
 }
