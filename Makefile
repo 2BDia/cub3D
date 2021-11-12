92150dddf5d38e16533733e96d319ced906ae880
Julie Van Audenhaege <rvan-aud@mi-c2-p2.s19.be>
2021-11-12 15:24:32 +0100

    fixed frees and error messages and makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-NAME		=	cub3d
+NAME		=	cub3D
 
 LIBFT_A		=	libft.a
 LIBFT_DIR	=	libft/
 LIBFT		=	$(addprefix $(LIBFT_DIR), $(LIBFT_A))
+
+MLX			=	mlx/libmlx.a
+MLX_DIR		=	mlx/
+LIB			=	-Lmlx -lz -lmlx -framework OpenGL -framework Appkit
 
 CC			=	gcc
 INCLUDE		=	includes
@@ -11,6 +15,7 @@ RM			=	rm -f
 
 SRCS_DIR	=	srcs
 PARSING_DIR	=	$(SRCS_DIR)/parsing
+RAYCAST_DIR	=	$(SRCS_DIR)/raycast
 
 SRCS		=	main.c 								\
 				$(SRCS_DIR)/free.c					\
@@ -19,22 +24,29 @@ SRCS		=	main.c 								\
 				$(PARSING_DIR)/modgnl_utils.c		\
 				$(PARSING_DIR)/modgnl.c				\
 				$(PARSING_DIR)/parsing.c			\
+				$(PARSING_DIR)/parsing_utils.c		\
 				$(PARSING_DIR)/read_file.c			\
+				$(RAYCAST_DIR)/raycast.c			\
+				$(SRCS_DIR)/key.c
 
 OBJS		=	$(SRCS:%.c=%.o)
 
 all:		$(NAME)
 
-$(NAME):	$(LIBFT) $(OBJS)
-			@$(CC) $(CFLAGS) -L$(LIBFT_DIR) -lft -lreadline -L/Users/$(USER)/.brew/opt/readline/lib -ltermcap $(OBJS) -o $(NAME)
+$(NAME):	$(MLX) $(LIBFT) $(OBJS)
+			@$(CC) $(CFLAGS) $(LIB) -L$(LIBFT_DIR) -lft $(OBJS) -o $(NAME)
 			@echo "\nLinked into executable \033[0;32mcub3d\033[0m."
 
 $(LIBFT):
-			@echo "Compiling libft.a"
+			@echo "Compiling Libft"
 			@$(MAKE) bonus -s -C $(LIBFT_DIR)
 
+$(MLX):
+			@echo "Compiling MiniLibX"
+			@$(MAKE) -s -C mlx
+
 .c.o:
-			@printf "\033[0;33mGenerating cub3d objects... %-33.33s\r" $@
+			@printf "\033[0;33mGenerating $(NAME) objects... %-33.33s\r" $@
 			@$(CC) $(CFLAGS) -c $< -o $(<:.c=.o)
 
 localclean:
@@ -44,10 +56,14 @@ localclean:
 clean:		localclean
 			@$(MAKE) clean -s -C $(LIBFT_DIR)
 			@echo "Clean libft."
+			@$(MAKE) clean -s -C $(MLX_DIR)
+			@echo "Clean MiniLibX."
 
 fclean:		localclean
 			@$(MAKE) fclean -s -C $(LIBFT_DIR)
 			@echo "Full clean libft."
+			@$(MAKE) fclean -s -C $(MLX_DIR)
+			@echo "Full clean MiniLibX."
 			@$(RM) $(NAME)
 			@echo "Removed executable."
 
--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -1,47 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 11:48:36 by rvan-aud          #+#    #+#             */
+/*   Updated: 2021/11/12 15:11:29 by rvan-aud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-int	get_map_height(char **map)
+static int	check_top_bot(char **map, int x, int y, int height)
 {
-	int	y;
+	int	start;
 
-	y = 0;
-	while (map[y])
-		y++;
-	return (y);
+	start = 0;
+	while (map[y][x] && x < (int)ft_strlen(map[y]))
+	{	
+		while (start == 0 && map[y][x] == ' ')
+			x++;
+		start = 1;
+		if (map[y][x] != '1' && map[y][x] != ' ')
+			return (1);
+		if (map[y][x] == ' ')
+			if (check_around(map, x, y, height))
+				return (1);
+		x++;
+	}
+	return (0);
 }
 
-int	check_around(char **map, int x, int y, int height)
+static int	check_middle_loop(char **map, int x, int y, int (*vars)[3])
 {
-	if (y == 0)
+	while ((*vars)[1] == 0 && map[y][x] == ' ')
+		x++;
+	if ((*vars)[1] == 0 && map[y][x] != '1')
+		return (-1);
+	(*vars)[1] = 1;
+	if (x == (int)ft_strlen(map[y]) - 1 && map[y][x] != '1')
+		return (-1);
+	if (map[y][x] != '0' && map[y][x] != '1' && map[y][x] != ' '
+		&& map[y][x] != 'N' && map[y][x] != 'S'
+		&& map[y][x] != 'W' && map[y][x] != 'E')
+		return (1);
+	if (map[y][x] == 'N' || map[y][x] == 'S'
+		|| map[y][x] == 'W' || map[y][x] == 'E')
 	{
-		if (map[y + 1][x] == '1' || map[y + 1][x] == ' ')
-			if (map[y][x - 1] == '1' || map[y][x - 1] == ' ')
-				if (map[y][x + 1] == '1' || map[y][x + 1] == ' ')
-					return (0);
+		if ((*vars)[2] || y == 0 || y == (*vars)[0] || x == 0
+			|| x == (int)ft_strlen(map[y]))
+			return (-1);
+		else
+			(*vars)[2] = 1;
 	}
-	else if (y == height)
-	{
-		if (map[y - 1][x] == '1' || map[y - 1][x] == ' ')
-			if (map[y][x - 1] == '1' || map[y][x - 1] == ' ')
-				if (map[y][x + 1] == '1' || map[y][x + 1] == ' ')
-					return (0);
-	}
-	else
-	{
-		if (map[y - 1][x] == '1' || map[y - 1][x] == ' ')
-			if (map[y + 1][x] == '1' || map[y + 1][x] == ' ')
-				if (map[y][x - 1] == '1' || map[y][x - 1] == ' ')
-					if (map[y][x + 1] == '1' || map[y][x + 1] == ' ')
-						return (0);
-	}
-	return (1);
+	if (map[y][x] == ' ')
+		if (check_around(map, x, y, (*vars)[0]))
+			return (-1);
+	x++;
+	return (x);
 }
 
-int	check_map(char **map)
+static int	check_middle(char **map, int x, int y, int height)
+{
+	int	start;
+	int	player;
+	int	vars[3];
+
+	start = 0;
+	player = 0;
+	vars[0] = height;
+	vars[1] = start;
+	vars[2] = player;
+	while (map[y][x] && x < (int)ft_strlen(map[y]))
+	{
+		x = check_middle_loop(map, x, y, &vars);
+		if (x == -1)
+			return (1);
+	}
+	return (0);
+}
+
+static int	check_map(char **map)
 {
 	int	x;
 	int	y;
-	int	start;
 	int	player;
 	int	height;
 
@@ -50,51 +93,16 @@ int	check_map(char **map)
 	height = get_map_height(map) - 1;
 	while (map[y])
 	{
-		start = 0;
 		x = 0;
 		if (y == 0 || y == height)
 		{
-			while (map[y][x] && x < (int)ft_strlen(map[y]))
-			{	
-				while (start == 0 && map[y][x] == ' ')
-					x++;
-				start = 1;
-				if (map[y][x] != '1' && map[y][x] != ' ')
-					return (1);
-				if (map[y][x] == ' ')
-					if (check_around(map, x, y, height))
-						return (1);
-				x++;
-			}
+			if (check_top_bot(map, x, y, height))
+				return (1);
 		}
 		else
 		{
-			while (map[y][x] && x < (int)ft_strlen(map[y]))
-			{
-				while (start == 0 && map[y][x] == ' ')
-					x++;
-				if (start == 0 && map[y][x] != '1')
-					return (1);
-				start = 1;
-				if (x == (int)ft_strlen(map[y]) - 1 && map[y][x] != '1')
-					return (1);
-				if (map[y][x] != '0' && map[y][x] != '1' && map[y][x] != ' '
-					&& map[y][x] != 'N' && map[y][x] != 'S'
-					&& map[y][x] != 'W' && map[y][x] != 'E')
-					return (1);
-				if (map[y][x] == 'N' || map[y][x] == 'S'
-					|| map[y][x] == 'W' || map[y][x] == 'E')
-				{
-					if (player || y == 0 || y == height || x == 0 || x == (int)ft_strlen(map[y]))
-						return (1);
-					else
-						player = 1;
-				}
-				if (map[y][x] == ' ')
-					if (check_around(map, x, y, height))
-						return (1);
-				x++;
-			}
+			if (check_middle(map, x, y, height))
+				return (1);
 		}
 		y++;
 	}
@@ -104,16 +112,18 @@ int	check_map(char **map)
 int	parsing(char *argv, t_file *file)
 {
 	file->raw_file = read_file(argv);
-	if (get_textures(file))
+	if (!file->raw_file)
 		return (1);
+	if (get_textures(file))
+		return (free_file(file));
 	if (get_map(file))
 		return (1);
 	if (check_map(file->map))
 	{
-		write(1, "wrong file\n", 10);
-		return (1);
+		write(2, "Invalid map\n", 12);
+		return (free_all(file));
 	}
 	if (get_pos(file))
-		return (1);
+		return (free_all(file));
 	return (0);
 }
